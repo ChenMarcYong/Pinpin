8cbd7f9cd1985c1d288da276d08d737f3ef8f7a0
chenmarcyong@gmail.com <chenmarcyong@gmail.com>
2024-12-18 12:20:04 +0100

    snowross

--- GetRealQuick2/Assets/Scripts/Ennemy/Snowross/Snowross_combat.cs
+++ GetRealQuick2/Assets/Scripts/Ennemy/Snowross/Snowross_combat.cs
@@ -6,17 +6,28 @@ public class Snowross_combat : MonoBehaviour
 {
     // Start is called before the first frame update
     [SerializeField] private GameObject snowball;
+
     [SerializeField] private Transform snowballSpawnPoint_up;
     [SerializeField] private Transform snowballSpawnPoint_right;
     [SerializeField] private Transform snowballSpawnPoint_down;
     [SerializeField] private Transform snowballSpawnPoint_left;
+
+    [SerializeField] private Transform snowballSpawnPoint_right_up_corner;
+    [SerializeField] private Transform snowballSpawnPoint_right_down_corner;
+    [SerializeField] private Transform snowballSpawnPoint_left_up_corner;
+    [SerializeField] private Transform snowballSpawnPoint_left_down_corner;
 
     private GameObject snowballInst_up;
     private GameObject snowballInst_right;
     private GameObject snowballInst_down;
     private GameObject snowballInst_left;
 
-    private float shootDirection = 1f;
+    private GameObject snowballInst_right_up_corner;
+    private GameObject snowballInst_right_down_corner;
+    private GameObject snowballInst_left_up_corner;
+    private GameObject snowballInst_left_down_cornert;
+
+    public float shootDirection = 1f;
 
     private float damage = 1f;
 
@@ -58,20 +69,20 @@ public class Snowross_combat : MonoBehaviour
 
             else 
             {
-                snowballInst_up = Instantiate(snowball, snowballSpawnPoint_up.position, Quaternion.Euler(0, 0, -45));
-                snowballInst_up.GetComponent<SnowballBehaviour>().SetShooter(gameObject, damage);
+                snowballInst_right_up_corner = Instantiate(snowball, snowballSpawnPoint_right_up_corner.position, Quaternion.Euler(0, 0, -135));
+                snowballInst_right_up_corner.GetComponent<SnowballBehaviour>().SetShooter(gameObject, damage);
 
                 // Haut droite
-                snowballInst_right = Instantiate(snowball, snowballSpawnPoint_right.position, Quaternion.Euler(0, 0, -135));
-                snowballInst_right.GetComponent<SnowballBehaviour>().SetShooter(gameObject, damage);
+                snowballInst_right_down_corner = Instantiate(snowball, snowballSpawnPoint_right_down_corner.position, Quaternion.Euler(0, 0, 135));
+                snowballInst_right_down_corner.GetComponent<SnowballBehaviour>().SetShooter(gameObject, damage);
 
                 // Bas gauche
-                snowballInst_down = Instantiate(snowball, snowballSpawnPoint_down.position, Quaternion.Euler(0, 0, 135));
-                snowballInst_down.GetComponent<SnowballBehaviour>().SetShooter(gameObject, damage);
+                snowballInst_left_up_corner = Instantiate(snowball, snowballSpawnPoint_left_up_corner.position, Quaternion.Euler(0, 0, -45));
+                snowballInst_left_up_corner.GetComponent<SnowballBehaviour>().SetShooter(gameObject, damage);
 
                 // Bas droite
-                snowballInst_left = Instantiate(snowball, snowballSpawnPoint_left.position, Quaternion.Euler(0, 0, 45));
-                snowballInst_left.GetComponent<SnowballBehaviour>().SetShooter(gameObject, damage);
+                snowballInst_left_down_cornert = Instantiate(snowball, snowballSpawnPoint_left_down_corner.position, Quaternion.Euler(0, 0, 45));
+                snowballInst_left_down_cornert.GetComponent<SnowballBehaviour>().SetShooter(gameObject, damage);
             }
 
 
