0ac422fb6f875e007ca1b2cc7ab092532c7a151e
chenmarcyong@gmail.com <chenmarcyong@gmail.com>
2024-12-19 17:11:26 +0100

    adding game over screen

--- GetRealQuick2/Assets/Scripts/UI/GameOverScreen.cs
+++ GetRealQuick2/Assets/Scripts/UI/GameOverScreen.cs
@@ -2,11 +2,17 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
 public class GameOverScreen : MonoBehaviour
 {
     public void setup() 
     {
         gameObject.SetActive(true);
+    }
+
+    public void restartButton() 
+    {
+        SceneManager.LoadScene("level_boss");
     }
 }
