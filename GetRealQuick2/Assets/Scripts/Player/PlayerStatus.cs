fc91291d0a5a1c873364dd6c0a471b1869990486
chenmarcyong@gmail.com <chenmarcyong@gmail.com>
2024-12-19 11:14:41 +0100

    add recovery time for pinpin

--- GetRealQuick2/Assets/Scripts/Player/PlayerStatus.cs
+++ GetRealQuick2/Assets/Scripts/Player/PlayerStatus.cs
@@ -4,14 +4,20 @@ using UnityEngine;
 
 public class PlayerStatus : MonoBehaviour
 {
+    private Animator animator;
 
+    [Header("Health Settings")]
     public float currentHealthPoint;
     public float MaxHealthPoint = 5f;
 
     private bool isAlreadyDead = false;
 
     public float attackDammage = 1f;
-    private Animator animator;
+    
+
+    [Header("Recovery Settings")]
+    public float recoveryTime = 1.0f; // Temps de récupération en secondes
+    private bool canTakeDamage = true;
 
     // Start is called before the first frame update
     void Start()
@@ -28,11 +34,19 @@ public class PlayerStatus : MonoBehaviour
 
     public void DamageTaken(float damage)
     {
-        if (!isAlreadyDead)
+        if (canTakeDamage && !isAlreadyDead)
         {
             animator.SetTrigger("Hurt");
             currentHealthPoint -= damage;
-        }
 
+            StartCoroutine(RecoveryCooldown());
+        }
+    }
+
+    private IEnumerator RecoveryCooldown()
+    {
+        canTakeDamage = false;
+        yield return new WaitForSeconds(recoveryTime);
+        canTakeDamage = true;
     }
 }
